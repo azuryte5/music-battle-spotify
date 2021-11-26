19487b5d6e0d5774b1d07e0c3fa078fee9d6fb34
Jess Hause Ugray <jess.ugray@gmail.com>
2021-11-26 18:12:21 -0500

    route to get a playlist - in progress

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 
+const apiRoutes = require('./api');
 const authenticationRoutes = require('./authentication-routes.js');
 
+router.use('/api', apiRoutes);
 router.use('/', authenticationRoutes);
+
+
 
 
 module.exports = router;
--- models/User.js
+++ models/User.js
@@ -1,22 +1,22 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+// const { Model, DataTypes } = require('sequelize');
+// const sequelize = require('../config/connection');
 
-// create our User model
-class User extends Model {}
+// // create our User model
+// class User extends Model {}
 
-User.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false
-    }, 
-  }
-);
+// User.init(
+//   {
+//     id: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       primaryKey: true,
+//       autoIncrement: true
+//     },
+//     username: {
+//       type: DataTypes.STRING,
+//       allowNull: false
+//     }, 
+//   }
+// );
 
-module.exports = User;
+// module.exports = User;
