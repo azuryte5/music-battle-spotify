d3e278491049b8e15a5d2d9b6a477f4c78de6826
Jess Hause Ugray <jess.ugray@gmail.com>
2021-11-25 19:10:48 -0500

    user model

---
+++ models/User.js
@@ -0,0 +1,22 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our User model
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }, 
+  }
+);
+
+module.exports = User;
